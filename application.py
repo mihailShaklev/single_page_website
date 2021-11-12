ff360a7400c592a049ccd00bb39430b814cc0eb4
Mihail Shaklev <60180238+mihailShaklev@users.noreply.github.com>
2021-11-12 17:25:34 +0200

    Update application.py

--- application.py
+++ application.py
@@ -7,8 +7,8 @@ mail= Mail(app)
 
 app.config['MAIL_SERVER']='smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
-app.config['MAIL_USERNAME'] = 'noreplytothis377@gmail.com'
-app.config['MAIL_PASSWORD'] = 'ggdtnufxeesuxwwj'
+app.config['MAIL_USERNAME'] = ''
+app.config['MAIL_PASSWORD'] = ''
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 mail = Mail(app)
